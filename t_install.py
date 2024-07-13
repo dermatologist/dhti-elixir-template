3f27615028e70d54e27bb914698a996df71361c2
Bell Eapen <github@gulfdoctor.net>
2024-07-13 09:48:48 -0500

    chore: Update imports in chain.py to use dhti_elixir_base module

---
+++ t_install.py
@@ -0,0 +1,13 @@
+from dhti_elixir_template.chain import chain
+from dhti_elixir_base import get_di
+
+
+_prompt = get_di("template_main_prompt")
+_llm = get_di("template_main_llm")
+
+
+# if you update this, you MUST also update ../pyproject.toml
+# with the new `tool.langserve.export_attr`
+chain = _prompt | _llm
+
+assert chain is not None
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,1 @@
-from dhti_elixir_template.bootstrap import bootstrap
-
-
-def pytest_configure(config):
-    print("Bootstrapping...")
-    bootstrap()
 
