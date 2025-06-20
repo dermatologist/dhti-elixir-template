028233fcf1d69749b9f53c434d957db5ed85c13e
dermatologist <github@gulfdoctor.net>
2025-06-20 08:06:42 -0500

    feat: Add a new template chain function for summarizing text input

--- src/dhti_elixir_template/chain.py
+++ src/dhti_elixir_template/chain.py
@@ -13,3 +13,17 @@ class TestChain(BaseChain):
         _chain = RunnablePassthrough() | get_di("template_main_prompt") | get_di("template_main_llm") | StrOutputParser() # type: ignore
         chain = _chain.with_types(input_type=self.input_type)
         return chain
+
+
+# Named chain according to the langchain template convention
+# The description is used by the agents
+# This is only in the inherited class, not in the base class
+@tool(TestChain().name or "test_chain", args_schema=TestChain().input_type)
+def chain(**kwargs):
+    """
+    This is a template chain that takes a text input and returns a summary of the text.
+
+    The input is a dict with the following mandatory keys:
+        input (str): The text to summarize.
+    """
+    return TestChain().chain.invoke(kwargs)
