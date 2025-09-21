10f8e886b132c4570fb21c552eda2860abbdeb44
dermatologist <github@gulfdoctor.net>
2025-09-21 06:54:04 -0500

    feat: Implement CDS hook discovery service and update endpoint routing

--- tests/server.py
+++ tests/server.py
@@ -2,6 +2,7 @@ import json
 from fastapi import FastAPI
 from langserve import add_routes
 from langchain_core.runnables.config import RunnableConfig
+from dhti_elixir_base import get_di
 
 # ! DO NOT REMOVE THE COMMENT BELOW
 # DHTI_CLI_IMPORT
@@ -61,36 +62,9 @@ async def invoke_chain(payload: dict):
 
 
 # https://cds-hooks.org/specification/current/#discovery
-@app.get("/cds-services")
+@app.get("/langserve/dhti_elixir_template/cds-services")
 async def read_root():
-    return {
-        "services": [
-            {
-                "hook": "patient-view",
-                "title": "Static CDS Service Example",
-                "description": "An example of a CDS Service that returns a static set of cards",
-                "id": "static-patient-greeter",
-                "prefetch": {"patientToGreet": "Patient/{{context.patientId}}"},
-            },
-            {
-                "hook": "order-select",
-                "title": "Order Echo CDS Service",
-                "description": "An example of a CDS Service that simply echoes the order(s) being placed",
-                "id": "order-echo",
-                "prefetch": {
-                    "patient": "Patient/{{context.patientId}}",
-                    "medications": "MedicationRequest?patient={{context.patientId}}",
-                },
-            },
-            {
-                "hook": "order-sign",
-                "title": "Pharmacogenomics CDS Service",
-                "description": "An example of a more advanced, precision medicine CDS Service",
-                "id": "pgx-on-order-sign",
-                "usageRequirements": "Note: functionality of this CDS Service is degraded without access to a FHIR Restful API as part of CDS recommendation generation.",
-            },
-        ]
-    }
+    return get_di("cds_hook_discovery") or { "services": [] }
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
