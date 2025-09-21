0be3f13b5df82ad5add1b7442b6706eed3f6a077
dermatologist <github@gulfdoctor.net>
2025-09-20 21:20:05 -0500

    feat: Enhance HTTP request structure and implement invoke_chain endpoint in server.py

--- tests/server.py
+++ tests/server.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import FastAPI
 from langserve import add_routes
 from langchain_core.runnables.config import RunnableConfig
@@ -42,6 +43,48 @@ except:
     # DHTI_NORMAL_ROUTE
     add_routes(app, dhti_elixir_template_chain, path="/langserve/dhti_elixir_template")
     x = True
+
+
+@app.post("/langserve/dhti_elixir_template/dhti")
+async def invoke_chain(payload: dict):
+
+    _input = {}
+    _input["input"] = {}
+    _input["input"]["input"] = payload
+    return dhti_elixir_template_chain(_input) # type: ignore
+
+
+# https://cds-hooks.org/specification/current/#discovery
+@app.get("/cds-services")
+async def read_root():
+    return {
+        "services": [
+            {
+                "hook": "patient-view",
+                "title": "Static CDS Service Example",
+                "description": "An example of a CDS Service that returns a static set of cards",
+                "id": "static-patient-greeter",
+                "prefetch": {"patientToGreet": "Patient/{{context.patientId}}"},
+            },
+            {
+                "hook": "order-select",
+                "title": "Order Echo CDS Service",
+                "description": "An example of a CDS Service that simply echoes the order(s) being placed",
+                "id": "order-echo",
+                "prefetch": {
+                    "patient": "Patient/{{context.patientId}}",
+                    "medications": "MedicationRequest?patient={{context.patientId}}",
+                },
+            },
+            {
+                "hook": "order-sign",
+                "title": "Pharmacogenomics CDS Service",
+                "description": "An example of a more advanced, precision medicine CDS Service",
+                "id": "pgx-on-order-sign",
+                "usageRequirements": "Note: functionality of this CDS Service is degraded without access to a FHIR Restful API as part of CDS recommendation generation.",
+            },
+        ]
+    }
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
