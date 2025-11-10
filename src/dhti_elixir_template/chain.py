6b8526d0d07dd2caba59c9c3a2810c0a75202834
dermatologist <github@gulfdoctor.net>
2025-11-09 19:03:25 -0600

    fix: Update dhti-elixir-base version to 1.2.0 in pyproject.toml; enhance bootstrap and chain functionality

--- src/dhti_elixir_template/chain.py
+++ src/dhti_elixir_template/chain.py
@@ -1,13 +1,12 @@
 import logging
 from typing_extensions import override
-
+from fhiry import FlattenFhir
 from dhti_elixir_base import BaseChain, get_di
-from dhti_elixir_base.cds_hook.generate_cards import add_card, get_card
+from dhti_elixir_base.cds_hook.generate_cards import get_card
 from dhti_elixir_base.cds_hook.request_parser import get_context
 from dhti_elixir_base.fhir.fhir_search import DhtiFhirSearch
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough, RunnableParallel
-from langchain.tools import tool
+from langchain_core.runnables import RunnablePassthrough
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -16,7 +15,6 @@ class DhtiChain(BaseChain):
 
     def print_log(self, message):
         logger.info(message)
-        print(message)
         return message
 
     def fhir_path_process(self, context: str):
@@ -29,16 +27,25 @@ class DhtiChain(BaseChain):
             self.print_log(f"Error in fhir_path_process: {e}")
             return "Demo working, but FHIR search failed."
 
+    def fhir_everything(self, context):
+        try:
+            _everything = DhtiFhirSearch().get_everything_for_patient(context)
+            if context.get("input"):
+                return f"Given the following FHIR resources: {FlattenFhir(_everything).flattened}, Answer the following question in a complete sentence: {context['input']}."
+            return str(FlattenFhir(_everything).flattened)
+        except Exception as e:
+            self.print_log(f"Error in fhir_everything: {e}")
+            return "Demo working, but FHIR search failed."
+
     @property
     @override
-    def chain(self): # type: ignore
-        _chain = RunnablePassthrough() | get_context | get_di("template_main_prompt") | get_di("template_main_llm") | StrOutputParser() | get_card # type: ignore
-        _fhir = (
+    def chain(self):  # type: ignore
+        _chain = (
             RunnablePassthrough()
             | get_context
-            | self.fhir_path_process
+            | self.fhir_everything
+            | get_di("template_main_llm")  # type: ignore
+            | StrOutputParser()
             | get_card
         )
-        # Run both in parallel using RunnableParallel
-        runnable = RunnableParallel(first=_chain, second=_fhir)
         return _chain.with_types(input_type=self.input_type)
